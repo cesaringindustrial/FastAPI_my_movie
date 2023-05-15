d37be7a97d2c7101944b69036791dea157227bd9
César Augusto Montañez E <cesaringindustrial@gmail.com>
2023-05-15 14:27:44 -0500

    changes on error handler

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from config.database import engine, Base
 from middlewares.error_handler import ErrorHandler
 from routers.movie import movie_router
 from routers.user import user_router
+import uvicorn, os
+
 app = FastAPI()
 app.title = "Mi app con FastAPI"
 app.version = "0.0.1"
@@ -21,3 +23,8 @@ Base.metadata.create_all(bind=engine)
 @app.get('/', tags= ['home'])
 def message():
     return HTMLResponse('<h1 hola mundo </h1>')
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0",
+                port=int(os.environ.get("PORT", 8000)))
+    

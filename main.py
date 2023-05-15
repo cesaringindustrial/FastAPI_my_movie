b8bd0a841fe7c04e6105e64a96feeacfc1a41177
César Augusto Montañez E <cesaringindustrial@gmail.com>
2023-05-15 14:36:39 -0500

    cambie puerto

--- main.py
+++ main.py
@@ -26,5 +26,4 @@ def message():
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0",
-                port=int(os.environ.get("PORT", 8000)))
-    
+                port=int(os.environ.get("PORT", 5000)))
